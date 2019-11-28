8128af7799497e7775a0cc313c93cbee1f3f1880
Vladimir <vladimirpeshkov5dev@yandex.ru>
2019-11-28 09:10:46 +0300

    update css and js default

--- src/resources/views/layouts/webflow/css-default.blade.php
+++ src/resources/views/layouts/webflow/css-default.blade.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 
 <!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/app.css') }}?{{ siteconf()->get("base-settings", "frontendDate", "") }}" rel="stylesheet">
 
 <style>
     .w-nav-menu[no-bootstrap] .dropdown-toggle::after {
--- src/resources/views/layouts/webflow/js-default.blade.php
+++ src/resources/views/layouts/webflow/js-default.blade.php
@@ -1,4 +1,4 @@
 <!-- APP JS -->
-<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/app.js') }}?{{ siteconf()->get("base-settings", "frontendDate", "") }}" type="text/javascript"></script>
 {{--<script src="{{ asset('js/form.js') }}" type="text/javascript"></script>--}}
 @stack('more-scripts')
